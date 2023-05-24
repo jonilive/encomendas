42e1d6bb2117f0d8188b4c5d78a2c51d827f1c94
João Rodrigues <jonilive@hotmail.com>
2023-05-24 08:37:33 +0100

    Updated jquery link in footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,4 +1,4 @@
-    <script src="<?php echo $SYSTEM_URL;?>js/jquery-3.4.1.min.js"></script>
+    <script src="<?php echo $SYSTEM_URL;?>js/jquery-3.7.0.min.js"></script>
     <script src="<?php echo $SYSTEM_URL;?>js/bootstrap.bundle.min.js"></script>
     <script src="<?php echo $SYSTEM_URL;?>js/bootstrap.min.js"></script>
     <script src="<?php echo $SYSTEM_URL;?>lib/DataTables/datatables.min.js"></script>
